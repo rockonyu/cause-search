8846a28bd204564a3502b11e00730c6eaf015a95
austin <rockonyu@gmail.com>
2025-04-20 01:00:38 +0800

    feat: 加入 Playwright E2E 測試

---
+++ tests/mocks/organizations/search.tsx
@@ -0,0 +1,85 @@
+export const search = {
+  data: [
+    {
+      id: "cm9nkf49e000amwksi0rzpda1",
+      name: "Will LLC",
+      description:
+        "Caelestis stella armarium tutamen repudiandae. Brevis conscendo vitae atrox beatus.",
+      thumbnail: "https://picsum.photos/seed/gLR57BrXde/2871/3582",
+      createdAt: "2025-04-17T03:59:23.735Z",
+    },
+    {
+      id: "cm9nkf49f002ymwksdbkckkxm",
+      name: "Terry - Baumbach",
+      description:
+        "Sordeo conicio constans uxor usque defero aperiam tabula supplanto. Tepesco voluptatem iusto cresco.",
+      thumbnail: "https://picsum.photos/seed/XaYtmcthmW/1740/1955",
+      createdAt: "2025-04-11T11:46:43.977Z",
+    },
+    {
+      id: "cm9nkf49e002lmwks77koxz5u",
+      name: "Shields Inc",
+      description:
+        "Cedo tum blanditiis tricesimus carpo concido subseco. Testimonium crepusculum civitas tibi cicuta nam arca vindico comprehendo.",
+      thumbnail: "https://picsum.photos/seed/QPNR5q2/3691/441",
+      createdAt: "2025-04-11T04:51:54.226Z",
+    },
+    {
+      id: "cm9nkf49e002jmwksr1wy106b",
+      name: "D'Amore - Ankunding",
+      description:
+        "Adinventitias tempus similique cunabula contego. Theca deprecator vicinus correptius nulla adversus corrupti.",
+      thumbnail: "https://loremflickr.com/961/1363?lock=349069794191008",
+      createdAt: "2025-04-08T23:37:07.330Z",
+    },
+    {
+      id: "cm9nkf49e0026mwks1vutmsv4",
+      name: "Wolf LLC",
+      description:
+        "Supplanto crinis copiose non victus articulus auditor curtus. Ullus tantum demonstro usus.",
+      thumbnail: "https://loremflickr.com/1136/337?lock=8803725237494810",
+      createdAt: "2025-04-07T19:38:21.856Z",
+    },
+    {
+      id: "cm9nkf49e001vmwkskj05emr8",
+      name: "Roob and Sons",
+      description:
+        "Aveho eum volaticus calcar angustus denique uter. Vacuus attero ustilo appello adaugeo cedo tui.",
+      thumbnail: "https://loremflickr.com/1797/24?lock=4685651840147045",
+      createdAt: "2025-04-01T22:51:53.428Z",
+    },
+    {
+      id: "cm9nkf49e002rmwks3zat478w",
+      name: "Kemmer and Sons",
+      description:
+        "Vis suppellex conculco porro. Desino creo aegrotatio textilis cognomen demoror adeptio iusto aduro tripudio.",
+      thumbnail: "https://loremflickr.com/1657/2535?lock=8790704454351625",
+      createdAt: "2025-03-30T04:24:10.417Z",
+    },
+    {
+      id: "cm9nkf49e0013mwksfu8fkhyn",
+      name: "Lesch - Schoen",
+      description:
+        "Eos dens nemo valetudo adeo arguo cursus truculenter. Comedo cubo quos.",
+      thumbnail: "https://loremflickr.com/2698/75?lock=1017026400543813",
+      createdAt: "2025-03-25T18:24:07.524Z",
+    },
+    {
+      id: "cm9nkf49e0008mwksuqzzv9ll",
+      name: "Kub, Rath and Smith",
+      description:
+        "Vere caelestis quis villa adimpleo amaritudo aveho caritas. Caput cruciamentum vinculum addo coniecto coniuratio universe caste.",
+      thumbnail: "https://picsum.photos/seed/7HtPcgkh8L/2688/1914",
+      createdAt: "2025-03-25T03:13:52.166Z",
+    },
+    {
+      id: "cm9nkf49e001tmwks28klu7kf",
+      name: "Prosacco Inc",
+      description:
+        "Tot aro stillicidium congregatio titulus atavus strues textus vacuus adflicto. Suffoco tres ademptio tolero adstringo tredecim bestia apparatus.",
+      thumbnail: "https://loremflickr.com/3825/3561?lock=7013157393243727",
+      createdAt: "2025-03-24T17:10:56.202Z",
+    },
+  ],
+  nextCursor: "2025-03-24T17:10:56.202Z",
+};
